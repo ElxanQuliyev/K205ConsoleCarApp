e4253ed55d4d4bacff345126c245242c375eb686
Elxan Quliyev <qelxan177@gmail.com>
2020-09-01 18:06:47 +0400

    Add project files.

---
+++ K205ConsoleCarApp/Car.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K205ConsoleCarApp
+{
+    class Car
+    {
+        public string Marka;
+        public string Model;
+        public decimal FuelEff;
+        public int MaxFuel;
+        public decimal CurrentFuel;
+        public decimal GlobalKm;
+        public decimal LocalKM;
+
+        public Car(string Marka, string Model, int FuelEff, int MaxFuel, int CurrentFuel = 80)
+        {
+            this.Marka = Marka;
+            this.Model = Model;
+            this.FuelEff = FuelEff;
+            this.MaxFuel = MaxFuel;
+            this.CurrentFuel = CurrentFuel;
+        }
+        public void GO()
+        {
+            Console.Write("Please km:");
+            string neededKm = Console.ReadLine();
+            if (IsNumber(neededKm))
+            {
+                decimal neededkmInt = Convert.ToDecimal(neededKm);
+                if (neededkmInt / 100 * FuelEff <= CurrentFuel)
+                {
+                    CurrentFuel -= neededkmInt / 100 * FuelEff;
+                    GlobalKm += neededkmInt;
+                    LocalKM += neededkmInt;
+                    Console.WriteLine($"Siz {neededkmInt} km yol getdiniz.Hal-hazirda {CurrentFuel}lr benzin qaldi");
+                    Console.WriteLine("Gedişi bitirmek üçün 1");
+                    string enddedKm = Console.ReadLine();
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Please topUp");
+                }
+
+            }
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"Local km:{LocalKM} GlobalKm:{GlobalKm}");
+        }
+        public void Reset()
+        {
+            LocalKM = 0;
+            Console.WriteLine($"Local km reset");
+        }
+
+        public void TopUp()
+        {
+            Console.Write("Please write lt:");
+            string neededFuel = Console.ReadLine();
+            if (IsNumber(neededFuel))
+            {
+                decimal needFuelDec = Convert.ToDecimal(neededFuel);
+                if (needFuelDec+CurrentFuel<=MaxFuel)
+                {
+                    CurrentFuel += needFuelDec;
+                    Console.WriteLine("Yaxsi yol");
+                }
+                else
+                {
+                    Console.WriteLine("Gozun doysun");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+
+        bool IsNumber(string number)
+        {
+            try
+            {
+                Convert.ToDecimal(number);
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ K205ConsoleCarApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K205ConsoleCarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car bmw = new Car("Bmw", "M5", 12, 80);
+            Car mer = new Car("Mercedes", "E-class", 12, 80);
+
+            string userMessage;
+            do
+            {
+
+                Console.WriteLine("Welcome We are Azpetrol");
+                Console.WriteLine("1.Go");
+                Console.WriteLine("2.TopUp");
+                Console.WriteLine("3.Stop");
+                Console.WriteLine("4.Reset Km");
+                Console.WriteLine("5.Exit");
+
+                userMessage = Console.ReadLine();
+                if (IsNumber(userMessage))
+                {
+                    switch (userMessage)
+                    {
+                        case "1":
+                            bmw.GO();
+                            break;
+
+                        case "2":
+                            bmw.TopUp();
+                            break;
+
+                        case "3":
+                            bmw.Stop();
+                            break;
+                        case "4":
+                            bmw.Reset();
+                            break;
+                        default:
+                            Console.WriteLine("Please only top number!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please write only number");
+                }
+
+            } while (userMessage != "5");
+        }
+
+        static bool IsNumber(string number)
+        {
+            try
+            {
+                Convert.ToInt32(number);
+                return true;
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
